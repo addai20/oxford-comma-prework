f7ac152d2c501c93cc063292a29f2f2cca681392
addai.5 <addai.5@buckeyemail.osu.edu>
2018-11-16 20:16:18 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,13 @@ def oxford_comma(array)
     return array[0]
   elsif array.length == 2 
     return array.join(" and ")
-  elsif array.length == 3
-    return array
+  elsif array.length > 2
+    first_part = array[0..-2]
+    last = array[-1]
+    last = "and" + last
+    
+    final_array = first_part.map { |ele| ele + ", "}
+    final_array << last
 end
+return final_array
 end
